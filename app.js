fc18ba832c42763bfc71a9a5cbff480c5ef6572e
Shubham <77922893+shubhamxdd@users.noreply.github.com>
2022-10-17 17:32:48 +0530

    second last

--- app.js
+++ app.js
@@ -1,74 +1,137 @@
-require("dotenv").config()
-const express = require("express")
-const bodyParser = require("body-parser")
-const ejs = require("ejs")
-const mongoose = require("mongoose")
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
 // const encrypt = require("mongoose-encryption")
 // const md5 = require("md5")
-const bcrypt = require("bcrypt")
-const saltRounds = 13
+// const bcrypt = require("bcrypt")
+// const saltRounds = 13
+const session = require("express-session")
+const passport = require("passport")
+const passportLocalMongoose = require("passport-local-mongoose")
 
-const app = express()
 
-console.log(process.env.SECRET);
+const app = express();
+
+// console.log(process.env.SECRET);
 // console.log(md5("123"));
 
-app.use(express.static("public"))
-app.set("view engine", "ejs")
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb://localhost:27017/userDB")
+app.use(session({
+    secret: "qwertyyuiop",
+    resave: false,
+    saveUninitialized: false
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+mongoose.connect("mongodb://localhost:27017/userDB");
 
 const userSchema = new mongoose.Schema({
-    email: String,
-    password: String
-})
+  email: String,
+  password: String,
+});
+
+userSchema.plugin(passportLocalMongoose)
 
 // userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]})
 
+const User = new mongoose.model("User", userSchema);
 
-const User = new mongoose.model("User", userSchema)
+passport.use(User.createStrategy())
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
 
 
-app.get("/", function(req, res){
-    res.render("home")
+app.get("/", function (req, res) {
+  res.render("home");
+});
+
+app.get("/login", function (req, res) {
+  res.render("login");
+});
+
+app.get("/register", function (req, res) {
+  res.render("register");
+});
+
+app.get("/secrets", function(req, res){
+  if(req.isAuthenticated()){
+    res.render("secrets")
+  }
+  else(
+    res.redirect("/login")
+  )
 })
 
-app.get("/login", function(req, res){
-    res.render("login")
+app.get("/logout", (req, res) => {
+  req.logout(req.user, err => {
+    if(err) return next(err);
+    res.redirect("/");
+  });
+});
+
+app.post("/register", function(req, res){
+  User.register({username: req.body.username}, req.body.password, function(err, user){
+    if(err){
+      console.log(err);
+      res.redirect("/register")
+    }
+    else{
+      passport.authenticate("local")(req, res, function(){
+        res.redirect("/secrets")
+      })
+    }
+  })
 })
 
-app.get("/register", function(req, res){
-    res.render("register")
+app.post("/login", function(req,res){
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password
+  })
+
+  req.login(user, function(err){
+    if(err){
+      console.log(err);
+    }
+    else{
+      passport.authenticate("local")(req, res, function(){
+        res.redirect("/secrets")
+      })
+    }
+  })
 })
 
-app.post("/register", function(req, res) {
-
-
-    bcrypt.hash(req.body.password, saltRounds, function(err, hash){
-
-        const newUser = new User({
-            email: req.body.username,
-            password: hash                        // md5(req.body.password)
-        })
-        newUser.save(function(err){
-            if(err){
-                console.log(err);
-            }
-            else{
-                res.render("secrets")
-            }
-        })
-    })
-    })
 
 
 
 
+/*
+app.post("/register", function (req, res) {
+  bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
+    const newUser = new User({
+      email: req.body.username,
+      password: hash, // md5(req.body.password)
+    });
+    newUser.save(function (err) {
+      if (err) {
+        console.log(err);
+      } else {
+        res.render("secrets");
+      }
+    });
+  });
+});
+*/
 
-
-
+/*
 app.post("/login", function(req,res){
     const username = req.body.username
     const password = req.body.password
@@ -90,13 +153,8 @@ app.post("/login", function(req,res){
         }
     )
 })
+*/
 
-
-
-
-
-
-
-app.listen("3000", function(){
-    console.log("Server is running on port 3000");
-})
+app.listen("3000", function () {
+  console.log("Server is running on port 3000");
+});
